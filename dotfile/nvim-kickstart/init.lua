9ac856444d5cad7713c7d5cd0faa0e713e519f87
evpeople <hangcaihui@gmail.com>
2024-03-26 00:34:00 +0800

    配置完crates

--- dotfile/nvim-kickstart/init.lua
+++ dotfile/nvim-kickstart/init.lua
@@ -238,7 +238,16 @@ require("lazy").setup({
 		"saecki/crates.nvim",
 		event = { "BufRead Cargo.toml" },
 		config = function()
-			require("crates").setup({})
+			require("crates").setup({
+				smart_insert = true,
+				popup = {
+					autofocus = true,
+				},
+				select = {
+					require_confirmation = true, -- 如果你想要选择后需要确认
+					-- 在这里配置其他 select 选项
+				},
+			})
 		end,
 	},
 	{
@@ -1310,28 +1319,34 @@ require("lazy").setup({
 	},
 })
 local crates = require("crates")
-local opts = { silent = true }
 
-vim.keymap.set("n", "<leader>ct", crates.toggle, opts)
-vim.keymap.set("n", "<leader>cr", crates.reload, opts)
+local map = function(keys, func, desc)
+	vim.keymap.set("n", keys, func, { desc = "crates: " .. desc })
+end
+map("<leader>ct", crates.toggle, "[C]rates [T]oggle 显示")
+map("<leader>cr", crates.reload, "[C]rates, [R]eload")
 
-vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, opts)
-vim.keymap.set("n", "<leader>cf", crates.show_features_popup, opts)
-vim.keymap.set("n", "<leader>cd", crates.show_dependencies_popup, opts)
+map("<leader>cv", crates.show_versions_popup, "[C]rates [V]erions")
+map("<leader>cf", crates.show_features_popup, "[C]rates [F]eatures")
+map("<leader>cd", crates.show_dependencies_popup, "[C]rates [D]ependence")
 
-vim.keymap.set("n", "<leader>cu", crates.update_crate, opts)
-vim.keymap.set("v", "<leader>cu", crates.update_crates, opts)
-vim.keymap.set("n", "<leader>ca", crates.update_all_crates, opts)
-vim.keymap.set("n", "<leader>cU", crates.upgrade_crate, opts)
-vim.keymap.set("v", "<leader>cU", crates.upgrade_crates, opts)
-vim.keymap.set("n", "<leader>cA", crates.upgrade_all_crates, opts)
+map("<leader>cu", crates.update_crate, "[C]rates [U]pdate")
+vim.keymap.set("v", "<leader>cu", crates.update_crates, { desc = "crates: update all  " })
+map("<leader>ca", crates.update_all_crates, "[C]rates update [A]ll")
+map("<leader>cU", crates.upgrade_crate, "[C]rates [U]pgrade")
+vim.keymap.set("v", "<leader>cU", crates.upgrade_crates, { desc = "crates upgrade all  " })
+map("<leader>cA", crates.upgrade_all_crates, "[C]reates, upgrade [A]ll")
 
-vim.keymap.set("n", "<leader>cx", crates.expand_plain_crate_to_inline_table, opts)
-vim.keymap.set("n", "<leader>cX", crates.extract_crate_into_table, opts)
+map(
+	"<leader>cx",
+	crates.expand_plain_crate_to_inline_table,
+	"将简单格式的 crate 声明扩展为内联表格格式。"
+)
+map("<leader>cX", crates.extract_crate_into_table, "将 crate 声明从任意格式提取到单独的表格格式。")
 
-vim.keymap.set("n", "<leader>cH", crates.open_homepage, opts)
-vim.keymap.set("n", "<leader>cR", crates.open_repository, opts)
-vim.keymap.set("n", "<leader>cD", crates.open_documentation, opts)
-vim.keymap.set("n", "<leader>cC", crates.open_crates_io, opts)
+map("<leader>cH", crates.open_homepage, "[C]rates [H]omepage")
+map("<leader>cR", crates.open_repository, "[C]rates [R]epostitory")
+map("<leader>cD", crates.open_documentation, "[C]rates [D]ocument")
+map("<leader>cC", crates.open_crates_io, "[C]rates open [C]rates.io")
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
