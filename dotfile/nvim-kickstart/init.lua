c217570b1cf945e02bd653fd72ff5c05d3a7bc92
evpeople <hangcaihui@gmail.com>
2024-03-27 14:42:23 +0800

    更新一些go的配置

--- dotfile/nvim-kickstart/init.lua
+++ dotfile/nvim-kickstart/init.lua
@@ -158,6 +158,35 @@ require("lazy").setup({
 		end,
 	},
 	{
+		"leoluz/nvim-dap-go",
+		config = true,
+	},
+	{
+		"nvim-neotest/neotest-go",
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		optional = true,
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = function(_, opts)
+					opts.ensure_installed = opts.ensure_installed or {}
+					vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl" })
+				end,
+			},
+		},
+		opts = function(_, opts)
+			local nls = require("null-ls")
+			opts.sources = vim.list_extend(opts.sources or {}, {
+				nls.builtins.code_actions.gomodifytags,
+				nls.builtins.code_actions.impl,
+				nls.builtins.formatting.goimports,
+				nls.builtins.formatting.gofumpt,
+			})
+		end,
+	},
+	{
 		"kdheepak/lazygit.nvim",
 		cmd = {
 			"LazyGit",
